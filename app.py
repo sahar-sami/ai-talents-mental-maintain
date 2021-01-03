46a2ab81c3ce3e9682927b6a917c6eff77ce8bba
BorthakurAYon <ab2535@cornell.edu>
2021-01-02 21:16:45 -0500

    mongoDB added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-# Import libraries
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pymongo
 import requests
 import pandas as pd
 import numpy as np
 import datetime
-from flask import Flask, request, jsonify
 import json
 import pickle
 import joblib
@@ -11,9 +12,6 @@ import os
 
 subject_id = 1.
 display_dict = {}
-
-app = Flask(__name__)
-
 
 def _load_test_data():
     """
@@ -28,10 +26,76 @@ def _load_test_data():
     return df[_idx, :].tolist()
 
 
-app = Flask(__name__)
-
 # Load the model
 model = joblib.load('models' + os.sep + 'model_sub' + str(subject_id) + '.pkl')
+
+# Replace your URL here. Don't forget to replace the password.
+connection_url = 'mongodb+srv://MentalMAIntain:ayon@mentalmaintain.toc5g.mongodb.net/MentalMAIntain?retryWrites=true&w=majority'
+app = Flask(__name__)
+client = pymongo.MongoClient(connection_url)
+
+# Database
+Database = client.get_database('MentalMAIntain')
+# Table
+StressData = Database.StressData
+
+# To insert a single document into the database,
+# insert_one() function is used
+@app.route('/insert-one/<_timestamp>/<_data>/<_label>/', methods=['GET'])
+def insertOne(_timestamp, _data, _label):
+    queryObject = {
+        'timestamp': _timestamp,
+        'data': _data,
+        'condition': _label,
+        'user_labelled': False,
+    }
+    query = StressData.insert_one(queryObject)
+    return "Query inserted...!!!"
+
+
+# To find the first document that matches a defined query,
+# find_one function is used and the query to match is passed
+# as an argument.
+@app.route('/find-one/<argument>/<value>/', methods=['GET'])
+def findOne(argument, value):
+    queryObject = {argument: value}
+    query = StressData.find_one(queryObject)
+    query.pop('_id')
+    return jsonify(query)
+
+
+# To find all the entries/documents in a table/collection,
+# find() function is used. If you want to find all the documents
+# that matches a certain query, you can pass a queryObject as an
+# argument.
+@app.route('/find/', methods=['GET'])
+def findAll():
+    # user_labelled
+    query = StressData.find()
+    output = {}
+    i = 0
+    for x in query:
+        if not x['user_labelled']:
+            output[i] = x
+            output[i].pop('_id')
+            i += 1
+    return jsonify(output)
+
+
+# To update a document in a collection, update_one()
+# function is used. The queryObject to find the document is passed as
+# the first argument, the corresponding updateObject is passed as the
+# second argument under the '$set' index.
+@app.route('/update/<key>/<value>/<element>/<updateValue>/', methods=['GET'])
+# key: 'timestamp'; element: 'condition'; updateValue: 'stress' or 'baseline' entered by user
+def update(key, value, element, updateValue):
+    queryObject = {key: value}
+    updateObject = {element: updateValue}
+    query = StressData.update_one(queryObject, {'$set': updateObject})
+    if query.acknowledged:
+        return "Update Successful"
+    else:
+        return "Update Unsuccessful"
 
 
 @app.route('/api', methods=['GET'])
@@ -51,6 +115,11 @@ def predict():
     display_dict.update({"success": True, "data": output, "Current date and time": now.strftime("%Y-%m-%d %H:%M:%S")})
     return json.dumps(display_dict)
 
+    # TODO:  call insert_one() to store the predicted data for user labelling
+    # TODO: When user labels a data
+    #  1. Call update first for http://0.0.0.0:8080/update/timestamp/../condition/..
+    #  2. Call update first for http://0.0.0.0:8080/update/timestamp/../user_labelled/True/
+    # TODO: Call findall() for displaying unlabelled data to the client
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=int(os.environ.get("PORT", 8080)))
